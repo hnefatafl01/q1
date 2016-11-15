1212c5caff7454cc5557a90246c1d1546c9717c2
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2016-11-15 11:49:40 -0700

    landing page

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ $(document).ready(function() {
   //hamburger menu toggle
   $('.mobile-nav').hide();
   $('span.nav-toggle').click(function() {
-    $('.mobile-nav').toggle();
-      // $('span.nav-toggle').hide();
+      $('.mobile-nav').toggle();
     });
   });
-  // $('.nav-toggle').click(function(e) {
-  //   $('.nav-toggle, .nav-item').toggleClass("is-active");
-  //   e.preventDefault();
-  //
-  // });
