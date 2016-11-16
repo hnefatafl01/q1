87a8502684d4f8598958e69bd80270525b9534a8
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2016-11-16 14:52:32 -0700

    create log page

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
   var url = "https://wger.de/api/v2/exercise/?language=2";
   // var key = "cd35b3ad5f20b8d5189acf17807c89685ac1584f";
   $.get(url, selectExercise);
-  function selectExercise(data){
+  function selectExercise(data) {
     for (i in data.results) {
       var exerciseName = data.results[i].name;
       $selectExercise.append("<option>" + exerciseName + "</option>");
